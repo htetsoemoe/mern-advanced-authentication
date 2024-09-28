eaba6064d2ce94dbb9b41f891934c8306e6cd106
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-09-28 06:44:27 +0630

    backend setup and run

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import path from 'path'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3500
+const __dirname = path.resolve()
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+
+app.use(express.json())
+app.use(cookieParser())
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello from advanced authentication project!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
